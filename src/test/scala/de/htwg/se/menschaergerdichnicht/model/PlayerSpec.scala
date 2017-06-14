d442aa8928bc271b27f7392306254e850ae91cf0
anhauser <anastasia.hauser@hotmail.com>
2017-06-14 10:53:34 +0200

    tests tests tests

--- src/test/scala/de/htwg/se/menschaergerdichnicht/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/menschaergerdichnicht/model/PlayerSpec.scala
@@ -7,4 +7,31 @@ import org.scalatest.{FlatSpec, Matchers}
   */
 class PlayerSpec extends FlatSpec with Matchers{
 
+  val player = new Player("test", 0)
+
+  "A Player" should "have an ID" in{
+    player.playerId
+  }
+  it should "be between 1 and 4" in{
+    assert(player.playerId >= 1 && player.playerId <= 4)
+  }
+
+  "A Player" should "have a House" in{
+    player.house
+  }
+
+  "A Player" should "have a target" in{
+    player.target
+  }
+
+  "A Player" should "have finished" in{
+    player.finished
+  }
+
+  "A Player" should "have tokens" in{
+    player.tokens
+  }
+
+
+
 }
